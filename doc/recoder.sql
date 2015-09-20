854604cae9afab1e3c5f20dc0d6b7c6d2b4de466
殇月陨 <410212983@qq.com>
2015-09-20 20:32:15 +0800

    核心静态页面

--- doc/recoder.sql
+++ doc/recoder.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
+Source Server         : localhost
 Source Server Version : 50145
 Source Host           : localhost:3306
 Source Database       : recoder
@@ -10,10 +10,30 @@ Target Server Type    : MYSQL
 Target Server Version : 50145
 File Encoding         : 65001
 
-Date: 2015-09-20 11:09:09
+Date: 2015-09-20 20:31:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `recoder`
+-- ----------------------------
+DROP TABLE IF EXISTS `recoder`;
+CREATE TABLE `recoder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text NOT NULL,
+  `createTime` datetime NOT NULL,
+  `creator` int(11) NOT NULL,
+  `tags` varchar(100) DEFAULT NULL,
+  `recoderType` int(11) DEFAULT NULL,
+  `flagDelete` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of recoder
+-- ----------------------------
+
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
@@ -30,4 +50,4 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO user VALUES ('1', 'syy', 'CE7F64B0D15B9B6A38420D22D81C15CE', '2015-09-20 10:53:11', '2015-09-20 10:55:22');
+INSERT INTO user VALUES ('1', 'syy', 'CE7F64B0D15B9B6A38420D22D81C15CE', '2015-09-20 10:53:11', '2015-09-20 14:35:25');
