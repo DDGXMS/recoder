8a27501d4a031863ee26121a82a6ea06bd26bb0b
殇月陨 <410212983@qq.com>
2015-10-06 10:55:11 +0800

    调整了页面布局；添加了标签筛选；

--- src/main/webapp/front/recoder/controller/recoderController.js
+++ src/main/webapp/front/recoder/controller/recoderController.js
@@ -1,5 +1,10 @@
 recoderApp.controller('RecoderController', function($rootScope, $scope, $http, $sce) {
 
+    $scope.curPage = 1;
+    $scope.maxPage = false;
+    // 标签checkbox选中
+    $scope.tagCheck = [];
+
     $rootScope.curTab = 'recoder';
     $scope.recoderList = new Array();
     $scope.tagMap = {};
@@ -50,6 +55,7 @@ recoderApp.controller('RecoderController', function($rootScope, $scope, $http, $
                     }
                 }
                 $scope.recoderList = data;
+                $scope.maxPage = false;
                 //$scope.recoderList = $scope.recoderList.concat(data);
 
                 $('.masonry-container').imagesLoaded( function () {
@@ -63,6 +69,32 @@ recoderApp.controller('RecoderController', function($rootScope, $scope, $http, $
                 alert(data.message);
             })
     }
+
+
+    $scope.$watch('query', function(oldValue, newValue){
+        console.log(newValue);
+        if (oldValue.keyword != newValue.keyword) {
+            return ;
+        }
+
+        $scope.queryData();
+    }, true);
+
+    // 将checkbox值转成id组合的字符串
+    $scope.$watchCollection('tagCheck', function () {
+        $scope.query.tags = '';
+        angular.forEach($scope.tagCheck, function (value, key) {
+            if (value) {
+                if (!$scope.query.tags) {
+                    $scope.query.tags += key;
+                } else {
+                    $scope.query.tags += ',' + key;
+                }
+            }
+        });
+    });
+
+
 
     $scope.queryData1 = function() {
         $http.get("/recoder/"+$scope.query.creator+"/"+$scope.query.pageNo, {params:$scope.query})
@@ -86,11 +118,37 @@ recoderApp.controller('RecoderController', function($rootScope, $scope, $http, $
             })
     }
 
-    $scope.$watch('query', function(oldValue, newValue){
-        if (oldValue.keyword != newValue.keyword) {
-            return ;
-        }
 
-        $scope.queryData();
-    }, true);
+    // 滚动到底部加载
+    $(window).scroll(function () {
+        if (!$scope.maxPage && ($(document).scrollTop() + $(window).height() >= $(document).height())) {
+            $scope.queryScroll = $.extend({}, $scope.query);
+            $scope.queryScroll.pageNo = $scope.curPage + 1;
+            $http.get("/recoder/"+$scope.queryScroll.creator+"/"+$scope.queryScroll.pageNo, {params:$scope.queryScroll})
+                .success(function(data) {
+                    if (!data || data.length == 0) {
+                        $scope.maxPage = true;
+                    } else {
+                        for (var index in data) {
+                            if (data[index].tags) {
+                                data[index]["tagList"] = data[index].tags.split(',');
+                            }
+                        }
+                        $scope.recoderList = $scope.recoderList.concat(data);
+
+                        $('.masonry-container').imagesLoaded( function () {
+                            $('.masonry-container').masonry({
+                                columnWidth: '.tile',
+                                itemSelector: '.tile'
+                            });
+                        });
+
+                        $scope.curPage++;
+                    }
+                })
+                .error(function(data) {
+                    alert(data.message);
+                })
+        }
+    });
 })
